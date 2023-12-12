b578b3b544c0f797835ecb2da3452913361951b6
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2023-12-11 19:43:52 -0700

    V1.0

---
+++ DominoV2/src/main/java/org/itson/dominov2/DominoV2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.dominov2;
+
+/**
+ *
+ * @author aleja
+ */
+public class DominoV2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
